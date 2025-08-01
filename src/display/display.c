d546972aabdf0a7cbc1999dd7808d4d97749cdf6
NicQ <nicquast@proton.me>
2025-08-01 20:29:45 +1000

    add function to clear display

--- src/display/display.c
+++ src/display/display.c
@@ -34,3 +34,12 @@ void deleteDisplay(DisplayHandle display_handle) {
 		free(display_handle);
 		display_handle = NULL;
 }
+
+// Takes the display handle and sets all pixels to 0
+void clearDisplay(DisplayHandle display_handle) {
+		for (int x = 0; x < DISPLAY_WIDTH; x++) {
+				for (int y = 0; y < DISPLAY_HEIGHT; y++) {
+						display_handle->pixelmap[x][y] = 0;
+				}
+		}
+}
