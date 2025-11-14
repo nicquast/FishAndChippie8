b9bcdd3b5ae1cee8684422a4897647a912293ccf
nicquast <nicquast@proton.me>
2025-11-14 22:13:51 +1100

    function to update display

--- src/display/display.c
+++ src/display/display.c
@@ -1,17 +1,28 @@
 #include "display.h"
 #include <SDL3/SDL_render.h>
+#include <SDL3/SDL_pixels.h>
 #include <stdlib.h>
 
-DisplayHandle createDisplay() {	
-    DisplayHandle display_handle = malloc(sizeof(Display));
+#define WHITE 0xFFFFFFFF
+#define BLACK 0xFF000000
 
+DisplayHandle createDisplay(SDL_Renderer* renderer) {
+    DisplayHandle display_handle = malloc(sizeof(Display));
     clearDisplay(display_handle);
+    display_handle->sdl_renderer = renderer;
+    display_handle->sdl_texture = SDL_CreateTexture(
+        renderer,
+        SDL_PIXELFORMAT_ARGB8888,
+        SDL_TEXTUREACCESS_STATIC,
+        DISPLAY_WIDTH,
+        DISPLAY_HEIGHT);
 
     return display_handle;
 }
 
 void deleteDisplay(DisplayHandle display_handle) {
     SDL_DestroyRenderer(display_handle->sdl_renderer);
+    SDL_DestroyTexture(display_handle->sdl_texture);
     free(display_handle);
     display_handle = nullptr;
 }
@@ -27,4 +38,22 @@ void clearDisplay(DisplayHandle display_handle) {
 
 void setPixel(DisplayHandle display_handle, const int x, const int y, const bool state) {
     display_handle->pixelmap[x][y] = state;
+}
+
+bool updateDisplay(DisplayHandle display_handle) {
+    u_int32_t* pixelBuffer = malloc(sizeof(u_int32_t) * DISPLAY_WIDTH * DISPLAY_HEIGHT);
+
+    for (int y = 0; y < DISPLAY_HEIGHT; y++) {
+        for (int x = 0; x < DISPLAY_WIDTH; x++) {
+            pixelBuffer[x + y * DISPLAY_WIDTH] = display_handle->pixelmap[x][y]
+                ? WHITE
+                : BLACK;
+        }
+    }
+
+    SDL_UpdateTexture(display_handle->sdl_texture, NULL, pixelBuffer, DISPLAY_WIDTH);
+
+    free(pixelBuffer);
+
+    return true;
 }
--- src/display/display.h
+++ src/display/display.h
@@ -9,18 +9,23 @@
 #define DISPLAY_HZ 60
 
 typedef struct {
-    bool pixelmap[DISPLAY_WIDTH][DISPLAY_HEIGHT]; 
+    bool pixelmap[DISPLAY_WIDTH][DISPLAY_HEIGHT]; //TODO: change to be an RGBA888 pixel buffer
     SDL_Renderer *sdl_renderer;
+    SDL_Texture *sdl_texture;
 } Display;
 
 typedef Display* DisplayHandle;
 
 // Initialise a display and return a handler for it
-DisplayHandle createDisplay();
+DisplayHandle createDisplay(SDL_Renderer*);
 
 // Clear the contents of a display
 void clearDisplay(DisplayHandle);
 
 // Delete the display and free its memory
 void deleteDisplay(DisplayHandle);
+
+// Update renderer attached to display to match current pixel grid
+// Returns true if successful, false if an SDL_Error has occurred
+bool updateDisplay(DisplayHandle);
 #endif
