7e2597cb8c785a71005795dc340ebf452028d429
nicquast <nicquast@proton.me>
2025-11-15 11:54:17 +1100

    Define struct to house registers

--- src/cpu/cpu.h
+++ src/cpu/cpu.h
@@ -7,7 +7,7 @@
 #include <sys/types.h>
 
 typedef u_int16_t mem_addr_t; // ToDo: 16bit int for 16 bit memory addresss - change actual type to a custom defined one later (after memory implementation)
-
+typedef u_int8_t byte_t;
 
 typedef struct Node {
     mem_addr_t memory_address;
@@ -25,5 +25,10 @@ mem_addr_t stackPop(Stack* stack);
 mem_addr_t stackPeek(Stack* stack);
 bool stackIsEmpty(Stack* stack);
 
+typedef struct {
+    mem_addr_t program_counter;
+    mem_addr_t index_register;
+    byte_t gp_registers[16];
+} RegisterStore;
 
-#endif //FISHANDCHIPPIE8_CPU_Hasd
+#endif //FISHANDCHIPPIE8_CPU_H
