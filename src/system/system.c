7c0679b20917e9d8d57a6e211e83056902328e66
nicquast <nicquast@proton.me>
2025-11-16 15:18:06 +1100

    Add system files to build system

---
+++ src/system/system.c
@@ -0,0 +1,3 @@
+//
+// Created by nic on 16/11/25.
+//
---
+++ src/system/system.h
@@ -0,0 +1,11 @@
+//
+// Created by nic on 16/11/25.
+//
+
+#ifndef FISHANDCHIPPIE8_SYSTEM_H
+#define FISHANDCHIPPIE8_SYSTEM_H
+
+#include "../memory/memory.h"
+#include "../cpu/cpu.h"
+
+#endif //FISHANDCHIPPIE8_SYSTEM_H
