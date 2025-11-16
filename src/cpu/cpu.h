434d028531fcb6c5bcedf265c9fe7a0b087c3683
nicquast <nicquast@proton.me>
2025-11-16 15:17:50 +1100

    Add function to initialise the register store struct.

--- src/cpu/cpu.h
+++ src/cpu/cpu.h
@@ -6,6 +6,8 @@
 #define FISHANDCHIPPIE8_CPU_H
 #include <sys/types.h>
 
+#define NUM_REGISTERS 16
+
 typedef u_int16_t mem_addr_t; // ToDo: 16bit int for 16 bit memory addresss - change actual type to a custom defined one later (after memory implementation)
 typedef u_int8_t byte_t;
 
@@ -28,7 +30,9 @@ bool stackIsEmpty(Stack* stack);
 typedef struct {
     mem_addr_t program_counter;
     mem_addr_t index_register;
-    byte_t gp_registers[16];
+    byte_t gp_registers[NUM_REGISTERS];
 } RegisterStore;
+
+RegisterStore initRegisterStore();
 
 #endif //FISHANDCHIPPIE8_CPU_H
