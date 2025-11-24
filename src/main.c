675f1f9fbf0bcae4ccbe8a75efc1b252d8956ad7
nicquast <nicquast@proton.me>
2025-11-24 18:56:43 +1100

    Load rom from commandline argument

--- src/main.c
+++ src/main.c
@@ -18,7 +18,12 @@
 #define WINDOW_WIDTH 800
 #define WINDOW_HEIGHT 400
 
-int main() {
+int main(int argc, char **argv) {
+
+	if (argc < 2) {
+		printf("Usage: %s [ROM file]\n", argv[0]);
+		return EXIT_FAILURE;
+	}
 	// Initialise SDL video components
 	if (!SDL_Init(SDL_FLAGS)) {
 		fprintf(stderr, "Error initialising SDL3: %s\n", SDL_GetError());
@@ -43,7 +48,14 @@ int main() {
 	DisplayHandle display_handle = createDisplay(renderer);
 	Chip8System chip8_system = initChip8System();
 
-	
+	//Load ROM
+	FILE *rom_fp = fopen(argv[1], "rb");
+	if (!rom_fp) {
+		fprintf(stderr, "Error opening ROM file: %s\n", argv[1]);
+		SDL_Quit();
+		return EXIT_FAILURE;
+	}
+	loadRom(rom_fp, chip8_system.memory);
 
 	SDL_DestroyWindow(window);
 	SDL_DestroyRenderer(renderer);
