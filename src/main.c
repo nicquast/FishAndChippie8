3422de3faed985975d4f01b5d0ca7702d597950b
nicquast <nicquast@proton.me>
2025-11-26 20:34:32 +1100

    Skeleton function to handle instructions

--- src/main.c
+++ src/main.c
@@ -18,6 +18,8 @@
 #define WINDOW_WIDTH 800
 #define WINDOW_HEIGHT 400
 
+void instructionTick(Chip8System *system);
+
 int main(int argc, char **argv) {
 
 	if (argc < 2) {
@@ -48,6 +50,8 @@ int main(int argc, char **argv) {
 	DisplayHandle display_handle = createDisplay(renderer);
 	Chip8System chip8_system = initChip8System();
 
+	printf("Chip8 system initialised\n");
+
 	//Load ROM
 	FILE *rom_fp = fopen(argv[1], "rb");
 	if (!rom_fp) {
@@ -57,10 +61,56 @@ int main(int argc, char **argv) {
 	}
 	loadRom(rom_fp, chip8_system.memory);
 
+	printf("Loaded ROM\n");
+
+	instructionTick(&chip8_system);
+
 	SDL_DestroyWindow(window);
 	SDL_DestroyRenderer(renderer);
 
 	SDL_Quit();
 
 	return EXIT_SUCCESS;
+}
+
+
+void instructionTick(Chip8System *system) {
+	//Fetch next instruction (also increments PC)
+	instruction_t c_inst = fetchInstruction(system);
+
+	//Deconstruct instruction
+	int x = getX(c_inst);
+	int y = getY(c_inst);
+	byte_t nn = getNN(c_inst);
+	mem_addr_t nnn = getNNN(c_inst);
+
+	printf("Instruction: %x\n", c_inst);
+
+	// Choose behaviour based on first nibble
+	switch (getNibble1(c_inst)) {
+	case 0x0:
+		// Differentiate between 00E0 and 00EE and execute
+		break;
+	case 0x1:
+		// Jump
+		break;
+	case 0x2:
+		// Call subroutine
+		break;
+	case 0x3:
+		// Skip conditionally
+		break;
+	case 0x4:
+		//Skip conditionally
+		break;
+	case 0x5:
+		//Skip conditionally
+		break;
+	case 0x9:
+		//Skip conditionally
+		break;
+	default:
+		break;
+	}
+
 }
