7fbfa28d7e505809d44045fed8df46dea3ec6ace
nic <nicquast@proton.me>
2026-02-07 22:44:36 +1100

    Create basic audio functions to beep

---
+++ src/audio/audio.c
@@ -0,0 +1,55 @@
+//
+// Created by nic on 7/2/26.
+//
+
+
+#include <SDL3/SDL_audio.h>
+#include <SDL3/SDL_log.h>
+
+// Number of float samples per second
+#define FLOAT_SAMPLES 8000
+// Minimum amount of audio to keep in the sound buffer (~0.5seconds)
+#define MIN_AUDIO ((FLOAT_SAMPLES * sizeof (float)) / 2)
+
+SDL_AudioSpec audio_spec;
+SDL_AudioStream *audio_stream;
+
+int current_sine_sample = 0;
+
+// SDL sound initialization
+void initSound() {
+    audio_spec.channels = 1;
+    audio_spec.freq = 44100;
+    audio_spec.format = SDL_AUDIO_F32;
+    audio_stream = SDL_OpenAudioDeviceStream(SDL_AUDIO_DEVICE_DEFAULT_PLAYBACK, &audio_spec, NULL, NULL);
+    if (!audio_stream) {
+        SDL_Log("Failed to open audio device stream: %s", SDL_GetError());
+    }
+}
+
+// Pause or unpause the audio
+void toggleSound(bool active) {
+    if (active) {
+        SDL_ResumeAudioStreamDevice(audio_stream);
+    } else {
+       SDL_PauseAudioStreamDevice(audio_stream);
+    }
+}
+
+//Push new audio to the buffer
+void updateSound() {
+   if (SDL_GetAudioStreamQueued(audio_stream) < MIN_AUDIO) {
+       static float samples[512];
+
+       for (int i = 0; i < SDL_arraysize(samples); i++) {
+           const int freq = 440;
+           const float phase = (float) current_sine_sample * (float) freq / (float) FLOAT_SAMPLES;
+           samples[i] = SDL_sinf(phase * 2 * SDL_PI_F);
+           current_sine_sample++;
+       }
+
+       current_sine_sample %= FLOAT_SAMPLES;
+
+       SDL_PutAudioStreamData(audio_stream, samples, sizeof(samples));
+   }
+}
---
+++ src/audio/audio.h
@@ -0,0 +1,12 @@
+//
+// Created by nic on 7/2/26.
+//
+
+#ifndef FISHANDCHIPPIE8_AUDIO_H
+#define FISHANDCHIPPIE8_AUDIO_H
+
+void initSound();
+void toggleSound(bool active);
+void updateSound();
+
+#endif //FISHANDCHIPPIE8_AUDIO_H
