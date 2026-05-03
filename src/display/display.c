a5a4348651eda9f171b265605916fd857f13b70e
nic <nicquast@proton.me>
2026-05-03 19:26:50 +1000

    Use variable on and off colours stored on the display instead of constants

--- src/display/display.c
+++ src/display/display.c
@@ -4,19 +4,21 @@
 #include <SDL3/SDL_render.h>
 #include <stdlib.h>
 
-#define ON_COLOUR 0xFFD7DEDC
-#define OFF_COLOUR 0xFF4B475C
 
 #define PIXEL_FORMAT SDL_PIXELFORMAT_ARGB8888
 
-DisplayHandle createDisplay(SDL_Renderer *renderer) {
+DisplayHandle createDisplay(SDL_Renderer *renderer, unsigned int on_colour, unsigned int off_colour) {
   DisplayHandle display_handle = malloc(sizeof(Display));
+  display_handle->on_colour = on_colour;
+  display_handle->off_colour = off_colour;
   clearDisplay(display_handle);
+
   display_handle->sdl_renderer = renderer;
   display_handle->sdl_texture =
       SDL_CreateTexture(renderer, PIXEL_FORMAT, SDL_TEXTUREACCESS_STATIC,
                         DISPLAY_WIDTH, DISPLAY_HEIGHT);
   SDL_SetTextureScaleMode(display_handle->sdl_texture, SDL_SCALEMODE_NEAREST);
+
   return display_handle;
 }
 
@@ -30,26 +32,23 @@ void deleteDisplay(DisplayHandle display_handle) {
 // Takes the display handle and sets all pixels to 0
 void clearDisplay(DisplayHandle display_handle) {
   for (int i = 0; i < DISPLAY_WIDTH * DISPLAY_HEIGHT; i++) {
-    display_handle->pixel_buffer[i] = OFF_COLOUR;
+    display_handle->pixel_buffer[i] = display_handle->off_colour;
   }
 }
 
 void setPixel(DisplayHandle display_handle, const int x, const int y,
               const bool state) {
   display_handle->pixel_buffer[x + y * DISPLAY_WIDTH] =
-      state ? ON_COLOUR : OFF_COLOUR;
+      state ? display_handle->on_colour : display_handle->off_colour;
 }
 
 int flipPixel(DisplayHandle display_handle, const int x, const int y) {
-  switch (display_handle->pixel_buffer[x + y * DISPLAY_WIDTH]) {
-  case ON_COLOUR:
-    display_handle->pixel_buffer[x + y * DISPLAY_WIDTH] = OFF_COLOUR;
+  if (display_handle->pixel_buffer[x + y * DISPLAY_WIDTH] == display_handle->on_colour) {
+    display_handle->pixel_buffer[x + y * DISPLAY_WIDTH] = display_handle->off_colour;
     return 1;
-  case OFF_COLOUR:
-    display_handle->pixel_buffer[x + y * DISPLAY_WIDTH] = ON_COLOUR;
+  } else {
+    display_handle->pixel_buffer[x + y * DISPLAY_WIDTH] = display_handle->on_colour;
     return 0;
-  default:
-    return -1;
   }
 }
 
